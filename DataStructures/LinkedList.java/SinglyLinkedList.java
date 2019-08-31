8ceafc2c0cc7ee6b8d46a7b32d39ca4e11de86c8
0x15F9 <isfaaqg@gmail.com>
2019-08-31 12:00:45 +0400

    Display LInklist

--- DataStructures/LinkedList.java/SinglyLinkedList.java
+++ DataStructures/LinkedList.java/SinglyLinkedList.java
@@ -4,9 +4,37 @@
 public class SinglyLinkedList {
     Node head;
 
-    // Create a singly linked list
+    /**
+     * Create a singly linked list
+     */
     public SinglyLinkedList() {
         this.head = null;
+    }
+
+    /**
+     * Create a singly linked list with an initial node
+     * 
+     * @param data
+     */
+    public SinglyLinkedList(int data) {
+        this.head = new Node(data);
+    }
+
+    /**
+     * Display the linked list
+     */
+    public void display() {
+        System.out.print("[");
+        display(this.head);
+    }
+
+    public void display(Node node) {
+        if (node != null) {
+            System.out.print(node.getData() + " > ");
+            display(node.getNextPointer());
+            return;
+        }
+        System.out.println("NULL]");
     }
 
 }
