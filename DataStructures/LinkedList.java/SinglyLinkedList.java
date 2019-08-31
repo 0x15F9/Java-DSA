0f2cfaf07045bbe0da36a7d9a32df12989bd15c7
0x15F9 <isfaaqg@gmail.com>
2019-08-31 11:20:43 +0400

    Create Singly linked list

---
+++ DataStructures/LinkedList.java/SinglyLinkedList.java
@@ -0,0 +1,12 @@
+/**
+ * SinglyLinkedList
+ */
+public class SinglyLinkedList {
+    Node head;
+
+    // Create a singly linked list
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+}
